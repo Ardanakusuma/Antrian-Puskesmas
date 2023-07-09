3cd03a7e49c91abf23cdad5a4d70139aecf8c04d
Ardanakusuma <97427520+Ardanakusuma@users.noreply.github.com>
2023-07-09 12:42:46 +0700

    Add files via upload

---
+++ N_2_AntrianPuskesmas/program/SQLTemplate.sql
@@ -0,0 +1,8 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
+ */
+/**
+ * Author:  bintangkusuma
+ * Created: 3 Jul 2023
+ */
